1e804987b47d9955a8622c28cd77fc5429fd1c7a
Iskander555 <neil.devan@gmail.com>
2016-08-21 18:42:57 +0530

    Add files via upload

---
+++ obstacle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class obstacle : MonoBehaviour {
+
+    public float speed = 1;	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += Vector3.down * speed * Time.deltaTime;
+       
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("trigger2");
+        if (gameObject.layer != 8)
+            Debug.Log("trigger1");
+        SceneManager.LoadScene("sceen2");
+
+    }
+}
